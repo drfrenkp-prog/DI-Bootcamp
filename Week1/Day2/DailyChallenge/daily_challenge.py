c1a867e780642fe383d16cef254317c13aa54a11
drfrenkp-prog <drfrenkp@gmail.com>
2026-08-06 15:12:35 +0300

    Add Day2 daily challenge - Multiples and Remove Duplicates

---
+++ Week1/Day2/DailyChallenge/daily_challenge.py
@@ -0,0 +1,13 @@
+# number = int(input("Enter a number: "))
+# length = int(input("Enter a length: "))
+# multiples = []
+# for i in range(1, length + 1):
+#     multiples.append(number * i)
+# print(multiples)
+
+word = input("Enter a word: ")
+result = ""
+for letter in word:
+    if result == "" or letter != result[-1]:
+        result += letter
+print(result)
