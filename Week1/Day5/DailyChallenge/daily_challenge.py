587e2fe25accbf2d437fa5c5a96615cb4c386f8c
drfrenkp-prog <drfrenkp@gmail.com>
2026-08-06 14:45:22 +0300

    Add second daily challenge - Pair sum finder

--- Week1/Day5/DailyChallenge/daily_challenge.py
+++ Week1/Day5/DailyChallenge/daily_challenge.py
@@ -5,14 +5,32 @@
 #print(result)
 
 
-def longest_word(sentence):
-    words = sentence.split()
-    longest = ""
-    for word in words:
-        if len(word) > len(longest):
-            longest = word
-    return longest
+#def longest_word(sentence):
+    #words = sentence.split()
+    #longest = ""
+    #for word in words:
+        #if len(word) > len(longest):
+            #longest = word
+    #return longest
 
-print(longest_word("Margaret's toy is a pretty doll."))
-print(longest_word("A thing of beauty is a joy forever."))
-print(longest_word("Forgetfulness is by all means powerless!"))
+#print(longest_word("Margaret's toy is a pretty doll."))
+#print(longest_word("A thing of beauty is a joy forever."))
+#print(longest_word("Forgetfulness is by all means powerless!"))
+
+
+import random
+
+list_of_numbers = [random.randint(0, 10000) for _ in range(20000)]
+target_number = 3728
+
+seen_numbers = set()
+found_pairs = []
+
+for number in list_of_numbers:
+    complement = target_number - number
+    if complement in seen_numbers:
+        found_pairs.append((number, complement))
+    seen_numbers.add(number)
+print(f"Found {len(found_pairs)} pairs that sum to {target_number}:")
+for pair in found_pairs[:5]:
+    print(pair)
